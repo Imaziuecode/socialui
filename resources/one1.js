dbe5d161680374a1a8724f65c3bbe660636701a2
Imaziuecode <122094223+Imaziuecode@users.noreply.github.com>
2023-02-06 11:26:30 +0530

    Update and rename one.json to one1.js

---
+++ resources/one1.js
@@ -0,0 +1,69 @@
+var suggestion_data=[
+  
+  
+  {
+      
+     "name":"Furqan Elahie",
+     "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2q_nNUGbQMj_dFa1EdSpJXirjWf_ikhLhLw&usqp=CAU",
+     "verified":true
+  },
+  {
+      
+     "name":"Zaineb Zaineb",
+     "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKI4RMNvFNF7JzODYd_69WmB7AkoEVkrpXgQ&usqp=CAU",
+     "verified":true
+  },
+  
+  {
+      
+     "name":"Angel Fariha",
+     "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRszNZBVoFbeAXerIDs6MbJIP03N8QM3UFmIw&usqp=CAU",
+     "verified":false
+  },
+  {
+      
+     "name":"Qurat Lone",
+     "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM8TyFTsCgZM3qFQq18IV4RKwgRIigFSeeFQ&usqp=CAU",
+     "verified":true
+  },
+  
+  
+  
+  ];
+  
+  
+  
+  
+  var story_data=[
+  
+  
+  {
+      
+     "name":"Furqan Elahie",
+     "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQhbCpkLu7m-ls-rO4oddL466Ta9jvPNaqZA&usqp=CAU",
+     
+  },
+  {
+      
+     "name":"Zaineb Zaineb",
+     "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQrXO8Po2MFbRjGUFCrcwV1ebMNbRotFLO3w&usqp=CAU",
+     
+  },
+  
+  {
+      
+     "name":"Angel Fariha",
+     "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRszNZBVoFbeAXerIDs6MbJIP03N8QM3UFmIw&usqp=CAU",
+     
+  },
+  {
+      
+     "name":"Qurat Lone",
+     "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNaqUoSTNkp1lbW5xLBKbTtLbW2SJPNI11RJq0baYhp6TlKpdBYyFbGY5H&s=10",
+     
+  },
+  
+  
+  
+  ];
+  
